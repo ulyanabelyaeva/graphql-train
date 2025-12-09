ccb85041e0461ce4e624d1a0e8e4606b5fd55296
Беляева Ульяна Дмитриевна <Belyaeva@innovasystem.pro>
2025-12-09 21:27:59 +0300

    add tests

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -6,12 +6,12 @@ insert into genre (id, name) values (1, 'Fantasy');
 insert into genre (id, name) values (2, 'Horror');
 insert into genre (id, name) values (3, 'Drama');
 
-insert into book (id, name, page_count, author_id) values (1, 'Harry Potter', 1030, 1);
-insert into book (id, name, page_count, author_id) values (2, 'Foo', 1030, 2);
-insert into book (id, name, page_count, author_id) values (3, 'Game of Thrones', 1030, 3);
-insert into book (id, name, page_count, author_id) values (4, 'Spring in Action', 1030, 2);
+insert into book (id, name, page_count, author_id) values (1000, 'Harry Potter', 1030, 1);
+insert into book (id, name, page_count, author_id) values (1001, 'Foo', 1030, 2);
+insert into book (id, name, page_count, author_id) values (1002, 'Game of Thrones', 1030, 3);
+insert into book (id, name, page_count, author_id) values (1003, 'Spring in Action', 1030, 2);
 
-insert into book_genre_relation (id, book_id, genre_id) values (1, 1, 1);
-insert into book_genre_relation (id, book_id, genre_id) values (2, 2, 2);
-insert into book_genre_relation (id, book_id, genre_id) values (3, 3, 3);
-insert into book_genre_relation (id, book_id, genre_id) values (4, 4, 1);
+insert into book_genre_relation (id, book_id, genre_id) values (1, 1000, 1);
+insert into book_genre_relation (id, book_id, genre_id) values (2, 1001, 2);
+insert into book_genre_relation (id, book_id, genre_id) values (3, 1002, 3);
+insert into book_genre_relation (id, book_id, genre_id) values (4, 1003, 1);
--- src/test/java/org/belyaeva/controller/BookControllerTest.java
+++ src/test/java/org/belyaeva/controller/BookControllerTest.java
@@ -1,24 +1,41 @@
 package org.belyaeva.controller;
 
 import org.belyaeva.dto.Book;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.test.tester.GraphQlTester;
 
-@GraphQlTest(BookController.class)
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureGraphQlTester
 class BookControllerTest {
 
     @Autowired
     private GraphQlTester graphQlTester;
 
-/*    @Test
-    void canGetBooks() {
-        graphQlTester.documentName("books") //указывает на /src/test/resources/books.graphql
+    @Test
+    void createNewBook() {
+        Long newBookId = graphQlTester.documentName("books")
+                .operationName("CreateNewBook")
                 .execute()
-                .path("books") //указывает на query books в books.graphql
-                .entityList(Book.class)
-                .hasSize(4);
+                .path("createNewBook")
+                .entity(Long.class)
+                .get();
+        Assertions.assertNotNull(newBookId);
+    }
 
-    }*/
+    @Test
+    void canGetBooks() {
+        List<Book> books = graphQlTester.documentName("books")
+                .operationName("GetAllBooks")
+                .execute()
+                .path("getAllBooks")
+                .entityList(Book.class)
+                .get();
+        Assertions.assertEquals(5, books.size());
+    }
 }
